e73ed69a1f84c69b3294d5ef0b8fce70357e2d41
Brian Nelson <brian8nelson@gmail.com>
2025-02-03 16:17:58 -0500

    Minor change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default function Layout() {
 	return (
 		<Container>
-			<h1 className="my-4">My Dog App</h1>
+			<h1 className="my-4 text-center fw-bolder text-primary">DogFinder</h1>
 			<Outlet />
 		</Container>
 	);
