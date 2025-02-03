4799701f418b556b5098397c6dae8ae220ab56a9
Brian Nelson <brian8nelson@gmail.com>
2025-02-03 16:43:26 -0500

    Added logout function to the layout that renders conditionally depending on the route

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,40 @@
-import { Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Container, Button } from "react-bootstrap";
+import api from "../api";
 
 export default function Layout() {
-	return (
-		<Container>
-			<h1 className="my-4 text-center fw-bolder text-primary">DogFinder</h1>
-			<Outlet />
-		</Container>
-	);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout");
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+
+  return (
+    <Container>
+      {location.pathname === "/search" && (
+        <div className="d-flex justify-content-between">
+          <div>
+						{/* TODO: Add user name */}
+					</div>
+          <div>
+            <Button
+              variant="outline-secondary"
+              onClick={handleLogout}
+              size="sm"
+            >
+              Logout
+            </Button>
+          </div>
+        </div>
+      )}
+      <h1 className="my-4 text-center fw-bolder text-primary">DogFinder</h1>
+      <Outlet />
+    </Container>
+  );
 }
