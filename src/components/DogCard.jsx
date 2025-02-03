750fef77391ed317d5e2f5ee26662ebefa34b1c0
Brian Nelson <brian8nelson@gmail.com>
2025-02-03 18:22:37 -0500

    Minor changes

--- src/components/DogCard.jsx
+++ src/components/DogCard.jsx
@@ -2,22 +2,24 @@ import React from "react";
 
 export default function DogCard({ dog }) {
 	return (
-		<div
-			style={{
-				border: "1px solid #ccc",
-				marginBottom: "1rem",
-				padding: "1rem",
-			}}
-		>
-			<img src={dog.img} alt={dog.name} style={{ width: "150px" }} />
-			<br />
-			<strong>Name:</strong> {dog.name}
-			<br />
-			<strong>Breed:</strong> {dog.breed}
-			<br />
-			<strong>Age:</strong> {dog.age}
-			<br />
-			<strong>ZIP Code:</strong> {dog.zip_code}
+		<div>
+			<img
+				src={dog.img}
+				alt={dog.name}
+				style={{ width: "100%", objectFit: "cover", marginBottom: "0.5rem" }}
+			/>
+			<div>
+				<strong>Name:</strong> {dog.name}
+			</div>
+			<div>
+				<strong>Breed:</strong> {dog.breed}
+			</div>
+			<div>
+				<strong>Age:</strong> {dog.age}
+			</div>
+			<div>
+				<strong>ZIP Code:</strong> {dog.zip_code}
+			</div>
 		</div>
 	);
 }
