74f1ef06c5cb514b319b54872086e026cc969bd8
Brian Nelson <brian8nelson@gmail.com>
2025-02-03 13:30:23 -0500

    Defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Search from './pages/Search'
 
 function App() {
   return (
-    <>
-      <h1>
-        Dog Finder
-      </h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
   )
 }
 
