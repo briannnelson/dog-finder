ddc72ea8123d3cecaf54f5341ee76fd5927061f7
Brian Nelson <brian8nelson@gmail.com>
2025-05-14 13:40:12 -0400

    Added fallback image for broken images

--- src/components/DogCard.jsx
+++ src/components/DogCard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default function DogCard({ dog }) {
+	const fallbackImg = new URL('/dog-thumbnail.png', window.location.origin).href;
 	return (
 		<div>
 			<img
 				src={dog.img}
 				alt={dog.name}
 				style={{ width: "100%", objectFit: "cover", marginBottom: "0.5rem", maxWidth: "220px" }}
+ 				onError={(e) => {
+					e.target.onerror = null;
+					e.target.src = fallbackImg;
+				}}
 			/>
 			<div>
 				<strong>Name:</strong> {dog.name}
